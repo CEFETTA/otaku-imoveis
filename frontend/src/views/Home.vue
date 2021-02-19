4e37f41003a3cb999341edf62c875788bead9f38
AllrightIsHere <gasilva912@gmail.com>
2021-02-18 21:41:09 -0300

    feat(frontend): add cards and pagination

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,183 @@
 <template>
-    <div class="container div-background">
-      <div class="row">
-        <div class="col-sm">
-          <div class="col-md-6 input-color">
-            <base-input alternative placeholder="Buscar Cidade" addon-left-icon="ni ni-zoom-split-in"></base-input>
+  <div>
+    <!-- Header -->
+    <div class="div-background" style="max-width: 100%; heigth: auto;">
+      <div class="container">
+        <div class="row mx-md-n5">
+          <div class="col-sm p-5 m-5 text-right">
             <base-input alternative placeholder="Buscar Bairro" addon-left-icon="ni ni-zoom-split-in"></base-input>
+            <button type="button p-5" class="btn btn-primary">Encontrar</button>
           </div>
-        </div>
-        <div class="col-sm">
-          <img class="div-img" src="../assets/images/logo.svg">
+          <div class="col-sm m-3">
+            <img class="div-img" src="../assets/images/logo.svg">
+          </div>
         </div>
       </div>
     </div>
+    <!-- Cards -->
+    <div class="row d-flex justify-content-center p-xl" style="max-width: 100%; heigth: auto;">
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col card-border p-3 m-3">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/images/logo.svg" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body row">
+              <div class="p-3">
+                <p class="col-sm h6">R$ 1.000,00</p>
+              </div>
+              <div>
+                <button type="button" class="btn btn-primary col-sm">Alugar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col card-border p-3 m-3">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/images/logo.svg" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body row">
+              <div class="p-3">
+                <p class="col-sm h6">R$ 1.000,00</p>
+              </div>
+              <div>
+                <button type="button" class="btn btn-primary col-sm">Alugar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col card-border p-3 m-3">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/images/logo.svg" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body row">
+              <div class="p-3">
+                <p class="col-sm h6">R$ 1.000,00</p>
+              </div>
+              <div>
+                <button type="button" class="btn btn-primary col-sm">Alugar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col card-border p-3 m-3">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/images/logo.svg" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body row">
+              <div class="p-3">
+                <p class="col-sm h6">R$ 1.000,00</p>
+              </div>
+              <div>
+                <button type="button" class="btn btn-primary col-sm">Alugar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col card-border p-3 m-3">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/images/logo.svg" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body row">
+              <div class="p-3">
+                <p class="col-sm h6">R$ 1.000,00</p>
+              </div>
+              <div>
+                <button type="button" class="btn btn-primary col-sm">Alugar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col card-border p-3 m-3">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/images/logo.svg" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body row">
+              <div class="p-3">
+                <p class="col-sm h6">R$ 1.000,00</p>
+              </div>
+              <div>
+                <button type="button" class="btn btn-primary col-sm">Alugar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Paginação -->
+    <div class="row d-flex justify-content-center">
+      <nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">
+              <i class="fa fa-angle-left"></i>
+              <span class="sr-only">Previous</span>
+            </a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active">
+            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              <i class="fa fa-angle-right"></i>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
 <script>
 
@@ -26,6 +192,7 @@ export default {
 <style>
   .div-background{
     background-image: url(../assets/images/background.png);
+    background-size: 100% 100%;
   }
 
   .div-img {
@@ -33,5 +200,10 @@ export default {
     max-height: 341px;
     width: auto;
     height: auto;
+  }
+
+  .card-border {
+    box-shadow: 9px 11px 36px rgba(0, 0, 0, 0.21);
+    border-radius: 6px;
   }
 </style>
