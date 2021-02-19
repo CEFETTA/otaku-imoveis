0ddbb2360078173fe6fa814b5f5e88380bc74344
AllrightIsHere <gasilva912@gmail.com>
2021-02-19 15:26:34 -0300

    feat(frontend): add navbar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <!-- Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#"><img src="../assets/images/logo-nav.svg"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#/register">Anunciar</a>
+        </div>
+      </div>
+    </nav>
     <!-- Header -->
     <div class="div-background" style="max-width: 100%; heigth: auto;">
       <div class="container">
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,43 +1,59 @@
 <template>
-    <section class="section section-xl my-0 section-background-img">
-        <div class="container ct-exemple-row">
-            <div class="row">
-                <div class="col-xl col-xl col-md col-sm"><span></span></div>
-                <div class="col-xl col-xl col-md col-sm text-center">
-                    <img class="section-div-img" src="../assets/images/logo.svg">
-                </div>
-                <div class="col-xl col-xl col-md col-sm"><span></span></div>
+    <div>
+        <!-- Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#" target="_blank"><img src="../assets/images/logo-nav.svg"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#" target="_blank">Home</a>
+                <a class="nav-item nav-link active" href="#/register">Anunciar<span class="sr-only">(current)</span></a>
             </div>
-        </div>
-        <div class="container pt-xl-md mt-4">
-            <div class="row justify-content-center">
-                <div class="col-xl-5">
-                    <card type="secondary" shadow
-                          header-classes="bg-white pb-5"
-                          body-classes="px-xl-5 py-xl-5"
-                          class="border-0">
-                        <template>
-                            <form role="form">
-                                <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Email"
-                                            addon-left-icon="ni ni-email-83">
-                                </base-input>
-                                <base-input alternative
-                                            type="password"
-                                            placeholder="Password"
-                                            addon-left-icon="ni ni-lock-circle-open">
-                                </base-input>
-                                <div class="text-center">
-                                    <base-button type="primary" class="my-4">Create</base-button>
-                                </div>
-                            </form>
-                        </template>
-                    </card>
+            </div>
+        </nav>
+        <!-- Registro -->
+        <section class="section section-xl my-0 section-background-img">
+            <div class="container ct-exemple-row">
+                <div class="row">
+                    <div class="col-xl col-xl col-md col-sm"><span></span></div>
+                    <div class="col-xl col-xl col-md col-sm text-center">
+                        <img class="section-div-img" src="../assets/images/logo.svg">
+                    </div>
+                    <div class="col-xl col-xl col-md col-sm"><span></span></div>
                 </div>
             </div>
-        </div>
-    </section>
+            <div class="container pt-xl-md mt-4">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5">
+                        <card type="secondary" shadow
+                            header-classes="bg-white pb-5"
+                            body-classes="px-xl-5 py-xl-5"
+                            class="border-0">
+                            <template>
+                                <form role="form">
+                                    <base-input alternative
+                                                class="mb-3"
+                                                placeholder="Email"
+                                                addon-left-icon="ni ni-email-83">
+                                    </base-input>
+                                    <base-input alternative
+                                                type="password"
+                                                placeholder="Password"
+                                                addon-left-icon="ni ni-lock-circle-open">
+                                    </base-input>
+                                    <div class="text-center">
+                                        <base-button type="primary" class="my-4">Create</base-button>
+                                    </div>
+                                </form>
+                            </template>
+                        </card>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 <script>
 export default {};
