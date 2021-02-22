411b1a28a8632ab56d593cbf759226886faf7033
Agronault <xande.luis@hotmail.com>
2021-02-22 10:18:30 -0300

    feat(logout): adds logout to router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Register from "./views/Register.vue";
 import Login from "./views/Login.vue";
+import Logout from "./views/components/SessionForms/Logout.vue";
 
 Vue.use(Router);
 
@@ -14,28 +15,35 @@ export default new Router({
       name: "home",
       components: {
         default: Home,
-      }
+      },
     },
     {
       path: "/register",
       name: "register",
       components: {
         default: Register,
-      }
+      },
     },
     {
       path: "/login",
       name: "login",
       components: {
         default: Login,
-      }
-    }
+      },
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      components: {
+        default: Logout,
+      },
+    },
   ],
-  scrollBehavior: to => {
+  scrollBehavior: (to) => {
     if (to.hash) {
       return { selector: to.hash };
     } else {
       return { x: 0, y: 0 };
     }
-  }
+  },
 });
