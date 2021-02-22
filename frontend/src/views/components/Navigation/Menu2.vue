96818665ff08c6834892974f6a7ac7eca3349574
Agronault <xande.luis@hotmail.com>
2021-02-22 10:19:07 -0300

    feat(navbar): adds login/register/logout options

--- frontend/src/views/components/Navigation/Menu2.vue
+++ frontend/src/views/components/Navigation/Menu2.vue
@@ -1,17 +1,29 @@
 <template>
-    <ul class="navbar-nav ml-lg-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="./#">Home
-                <span class="sr-only">(current)</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./#/register">Anunciar</a>
-        </li>
-    </ul>
+  <ul class="navbar-nav ml-lg-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="./#"
+        >Home
+        <span class="sr-only">(current)</span>
+      </a>
+    </li>
+    <li class="nav-item" v-if="!loggedIn">
+      <a class="nav-link" href="./#/login">Entrar</a>
+    </li>
+    <li class="nav-item" v-if="!loggedIn">
+      <a class="nav-link" href="./#/register">Registrar</a>
+    </li>
+    <li class="nav-item" v-if="!!loggedIn">
+      <a class="nav-link" href="./#/logout">Sair</a>
+    </li>
+  </ul>
 </template>
 <script>
-export default {};
+export default {
+  computed: {
+    loggedIn() {
+      return this.$store.getters.checkLogin;
+    },
+  },
+};
 </script>
-<style>
-</style>
+<style></style>
