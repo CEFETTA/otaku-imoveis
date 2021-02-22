76a9c5d67532d6b541e12923710c5f869b5747cf
Agronault <xande.luis@hotmail.com>
2021-02-22 10:15:14 -0300

    refactor: centralizes endpoint handling

--- frontend/src/views/components/SessionForms/js/session.js
+++ frontend/src/views/components/SessionForms/js/session.js
@@ -1,21 +1,20 @@
-import apiUri from "../../../../config/uri";
 import axios from "axios";
 
 export async function performRegister(name, email, password) {
-  const registerEndpoint = `${apiUri}/users`;
+  const registerEndpoint = `http://localhost:3333/users`;
   const body = { name, email, password };
 
   return await axios.post(registerEndpoint, body);
 }
 
 export async function performLogin(email, password) {
-  const loginEndpoint = `${apiUri}/sessions`;
+  const loginEndpoint = `http://localhost:3333/sessions`;
   const body = { email, password };
 
   return await axios.post(loginEndpoint, body);
 }
 
 export function getAuthHeader() {
-  const user = JSON.parse(localStorage.getItem("user"));
-  return user && user.token ? { Authorization: `Bearer ${user.token}` } : {};
+  const token = JSON.parse(localStorage.getItem("token"));
+  return token ? { Authorization: `Bearer ${token}` } : {};
 }
