a5d646b1017227843e4bd7195ef8d112a471ca85
Agronault <xande.luis@hotmail.com>
2021-02-22 23:26:17 -0300

    feat(register): adds register integration

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,68 +1,55 @@
 <template>
-    <div>
-        <!-- Registro -->
-        <section class="section section-xl my-0 section-background-img">
-            <div class="container ct-exemple-row">
-                <div class="row">
-                    <div class="col-xl col-xl col-md col-sm"><span></span></div>
-                    <div class="col-xl col-xl col-md col-sm text-center">
-                        <img class="section-div-img" src="../assets/images/logo.svg">
-                    </div>
-                    <div class="col-xl col-xl col-md col-sm"><span></span></div>
-                </div>
-            </div>
-            <div class="container pt-xl-md mt-4">
-                <div class="row justify-content-center">
-                    <div class="col-xl-5">
-                        <card type="secondary" shadow
-                            header-classes="bg-white pb-5"
-                            body-classes="px-xl-5 py-xl-5"
-                            class="border-0">
-                            <template>
-                                <form role="form">
-                                    <base-input alternative
-                                                class="mb-3"
-                                                placeholder="Email"
-                                                addon-left-icon="ni ni-email-83">
-                                    </base-input>
-                                    <base-input alternative
-                                                type="password"
-                                                placeholder="Password"
-                                                addon-left-icon="ni ni-lock-circle-open">
-                                    </base-input>
-                                    <div class="text-center">
-                                        <base-button type="primary" class="my-4">Create</base-button>
-                                    </div>
-                                </form>
-                            </template>
-                        </card>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+  <div>
+    <!-- Registro -->
+    <section class="section section-xl my-0 section-background-img">
+      <div class="container ct-exemple-row">
+        <div class="row">
+          <div class="col-xl col-xl col-md col-sm"><span></span></div>
+          <div class="col-xl col-xl col-md col-sm text-center">
+            <img class="section-div-img" src="../assets/images/logo.svg" />
+          </div>
+          <div class="col-xl col-xl col-md col-sm"><span></span></div>
+        </div>
+      </div>
+      <div class="container pt-xl-md mt-4">
+        <div class="row justify-content-center">
+          <div class="col-xl-5">
+            <card
+              type="secondary"
+              shadow
+              header-classes="bg-white pb-5"
+              body-classes="px-xl-5 py-xl-5"
+              class="border-0"
+            >
+              <register-form />
+            </card>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
-
+import RegisterForm from "./components/SessionForms/RegisterForm";
 export default {
-    components: {
-    },
-    data() {
-        return {
-        }
-    }
+  components: {
+    RegisterForm,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 <style>
-    .section-background-img {
-        background-image: url(../assets/images/background.png);
-        background-size: 100% 100%;
-    }
+.section-background-img {
+  background-image: url(../assets/images/background.png);
+  background-size: 100% 100%;
+}
 
-    .section-div-img {
-        max-width: 247px;
-        max-height: 341px;
-        width: auto;
-        height: auto;
-    }
+.section-div-img {
+  max-width: 247px;
+  max-height: 341px;
+  width: auto;
+  height: auto;
+}
 </style>
